aa80d9d518a80917ada53fbdc957e65aebb8a11e
Kiran Awate <awatekiran@outlook.com>
2021-03-18 01:06:47 -0700

    initial commit

---
+++ create_graph.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+#
+## change directory to the rrdtool script dir
+cd /Users/kawate/repos/checknetfi/
+ 
+## Graph for last 24 hours 
+/usr/local/bin/rrdtool graph latency_graph.png \
+-w 785 -h 120 -a PNG \
+-c CANVAS#231E1E \
+-c FONT#C1F588 \
+-c BACK#231E1E \
+-c AXIS#C1F588 \
+--slope-mode \
+--start -86400 --end now \
+--font DEFAULT:8: \
+--title "ping google.com" \
+--watermark "`date`" \
+--vertical-label "latency(ms)" \
+--right-axis-label "latency(ms)" \
+--lower-limit 0 \
+--right-axis 1:0 \
+--x-grid MINUTE:10:HOUR:1:MINUTE:120:0:%R \
+--alt-y-grid --rigid \
+DEF:roundtrip=ping_db.rrd:rtt:MAX \
+DEF:packetloss=ping_db.rrd:pl:MAX \
+CDEF:PLNone=packetloss,0,0,LIMIT,UN,UNKN,INF,IF \
+CDEF:PL10=packetloss,1,10,LIMIT,UN,UNKN,INF,IF \
+CDEF:PL25=packetloss,10,25,LIMIT,UN,UNKN,INF,IF \
+CDEF:PL50=packetloss,25,50,LIMIT,UN,UNKN,INF,IF \
+CDEF:PL100=packetloss,50,100,LIMIT,UN,UNKN,INF,IF \
+LINE1:roundtrip#99CC33:"latency(ms)" \
+GPRINT:roundtrip:LAST:"Cur\: %5.2lf" \
+GPRINT:roundtrip:AVERAGE:"Avg\: %5.2lf" \
+GPRINT:roundtrip:MAX:"Max\: %5.2lf" \
+GPRINT:roundtrip:MIN:"Min\: %5.2lf\t" \
+COMMENT:"pkt loss\:" \
+AREA:PLNone#231E1E:"0%":STACK \
+AREA:PL10#FFC100:"1-10%":STACK \
+AREA:PL25#FF7400:"10-25%":STACK \
+AREA:PL50#FF4D00:"25-50%":STACK \
+AREA:PL100#FF0000:"50-100%":STACK
+ 
+## copy to the web directory
+# cp latency_graph.png /var/www/htdocs/
---
+++ update_rrd.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+#
+### set the paths
+command="/sbin/ping -q -n -c 45"
+gawk="/usr/local/bin/gawk"
+rrdtool="/usr/local/bin/rrdtool"
+hosttoping="google.com"
+ 
+### data collection routine 
+get_data() {
+    local output=$($command $1 2>&1)
+    local method=$(echo "$output" | $gawk '
+        BEGIN {pl=100; rtt=0.1}
+        /packets transmitted/ {
+            match($0, /([0-9]+)% packet loss/, datapl)
+            pl=datapl[1]
+        }
+        /min\/avg\/max/ {
+            match($4, /(.*)\/(.*)\/(.*)\/(.*)/, datartt)
+            rtt=datartt[2]
+        }
+        END {print pl ":" rtt}
+        ')
+    echo $method
+    RETURN_DATA=$method
+}
+ 
+### change to the script directory
+cd /Users/kawate/repos/checknetfi/
+ 
+### collect the data
+get_data $hosttoping
+ 
+### update the database
+$rrdtool update ping_db.rrd --template pl:rtt N:$RETURN_DATA
